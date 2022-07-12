7ff21326d3ad1c4dbd0b68cb338a70991520cfd3
Chris Guy <chris@chrisguy.co>
2022-07-12 16:52:39 +0100

    Upgrade to Python 3.10.4. Remove ImageChooserPanel and StreamFieldPanel refs

--- home/models.py
+++ home/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from wagtail import images
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
-from wagtail.images.edit_handlers import ImageChooserPanel
 from django.shortcuts import get_object_or_404
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
@@ -15,7 +14,6 @@ class HomePage(SocialMetaMixin, Page):
     max_count = 1
     home_title = models.CharField(max_length=250, null=True, blank=True)
     intro = RichTextField(blank=True)
-    # noinspection PyUnresolvedReferences
     hero = models.ForeignKey(
         images.get_image_model(), null=True, blank=True,
         on_delete=models.SET_NULL, related_name='+'
@@ -26,14 +24,15 @@ class HomePage(SocialMetaMixin, Page):
             ('preview_pane_block', blocks.PreviewPaneBlock()),
         ],
         null=True,
-        blank=True
+        blank=True,
+        use_json_field=True,
     )
 
     content_panels = Page.content_panels + [
         FieldPanel('home_title'),
         FieldPanel('intro', classname="full"),
-        ImageChooserPanel('hero'),
-        StreamFieldPanel('content'),
+        FieldPanel('hero'),
+        FieldPanel('content'),
     ]
 
 
