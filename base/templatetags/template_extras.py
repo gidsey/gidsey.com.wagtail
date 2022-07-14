51e9c7cffb53c82ed18d06680267419e1d022855
Chris Guy <chris@chrisguy.co>
2022-07-14 15:48:56 +0100

    set jpeg quality to 70

--- base/templatetags/template_extras.py
+++ base/templatetags/template_extras.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter
 def social_url(img):
-    return img.get_rendition('fill-1200x630|jpegquality-60').url
+    return img.get_rendition('fill-1200x630|jpegquality-70').url
 
 
 @register.filter
